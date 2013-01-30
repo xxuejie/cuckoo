5f552b901b08d8bcdec54cdf73a6113d1f03b18b
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-01-29 20:45:42 -0500

    Add user view

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,7 +4,28 @@
 var CuckooHomeCtrl = ['$scope', function($scope) {
 }];
 
-var CuckooUserCtrl = ['$scope', function($scope) {
+var CuckooUserCtrl = ['$scope', '$routeParams', 'User', function($scope, $routeParams, User) {
+  $scope.user = User.get({userId: $routeParams.id}, function(user) {
+  });
+
+  $scope.toggleFollow = function(user) {
+    // implement a http request call here
+    console.log("Toogle follow status for user: " + user.login_name);
+
+    user.followed = !user.followed;
+  };
+
+  $scope.getFollowText = function(followed) {
+    return followed ? "Followed" : "Follow";
+  }
+
+  $scope.getFollowClass = function(followed) {
+    return followed ? "btn-primary" : "";
+  }
+
+  $scope.formatTime = function(t) {
+    return moment(new Date(parseInt(t))).fromNow();
+  }
 }];
 
 var CuckooUserListCtrl = [ '$scope', '$http', function($scope, $http) {
--- public/js/services.js
+++ public/js/services.js
@@ -5,5 +5,8 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('cuckooApp.services', []).
-  value('version', '0.1');
+angular.module('cuckooApp.services', ['ngResource']).
+  value('version', '0.1').
+  factory('User', function($resource) {
+    return $resource('api/user/:userId.json', {}, {});
+  });
