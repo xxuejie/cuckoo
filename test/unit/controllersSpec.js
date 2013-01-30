e1887be93eaf9585f4510455172e48d137ec9551
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-01-29 22:36:27 -0500

    Fix test cases. Note: this piece of code looks so much like Guice, is this a coincidence?

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,53 +2,63 @@
 
 /* jasmine specs for controllers go here */
 
-describe('CuckooHomeCtrl', function(){
-  var homeCtrl, scope;
+describe('Cuckoo controllers', function() {
 
-  beforeEach(inject(function($controller, $rootScope) {
-    scope = $rootScope.$new();
-    homeCtrl = $controller(CuckooHomeCtrl, {$scope: scope});
-  }));
+  beforeEach(module('cuckooApp.services'));
+
+  describe('CuckooHomeCtrl', function(){
+    var homeCtrl, scope;
+
+    beforeEach(inject(function($controller, $rootScope) {
+      scope = $rootScope.$new();
+      homeCtrl = $controller(CuckooHomeCtrl, {$scope: scope});
+    }));
 
 
-  it('should ....', function() {
-    //spec body
+    it('should ....', function() {
+      //spec body
+    });
   });
-});
 
 
-describe('CuckooUserCtrl', function(){
-  var userCtrl, scope;
+  describe('CuckooUserCtrl', function(){
+    var userCtrl, scope, routeParams, $httpBackend;
 
-  beforeEach(inject(function($controller, $rootScope) {
-    scope = $rootScope.$new();
-    userCtrl = $controller(CuckooUserCtrl, {$scope: scope});
-  }));
+    beforeEach(inject(function($controller, $rootScope, _$httpBackend_) {
+      scope = $rootScope.$new();
+      routeParams = {"id": 1};
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('api/user/1.json').
+        respond({"id": 1});
+      userCtrl = $controller(CuckooUserCtrl, {$scope: scope,
+                                              $routeParams: routeParams});
+    }));
 
-  it('should ....', function() {
-    //spec body
+    it('should ....', function() {
+      //spec body
+    });
   });
-});
 
-describe('CuckooUserListCtrl', function() {
-  var scope, ctrl, $httpBackend;
+  describe('CuckooUserListCtrl', function() {
+    var scope, ctrl, $httpBackend;
 
-  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('api/users.json').
-      respond([{"login_name": "test_user",
-                "avatar": "http://www.gravatar.com/avatar/00000000000000000000000000000000",
-                "description": "I'm just an evil test account-_-",
-                "followed": false}]);
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('api/users.json').
+        respond([{"login_name": "test_user",
+                  "avatar": "http://www.gravatar.com/avatar/00000000000000000000000000000000",
+                  "description": "I'm just an evil test account-_-",
+                  "followed": false}]);
 
 
-    scope = $rootScope.$new();
-    ctrl = $controller(CuckooUserListCtrl, {$scope: scope});
-  }));
+      scope = $rootScope.$new();
+      ctrl = $controller(CuckooUserListCtrl, {$scope: scope});
+    }));
 
-  it('should have one user', function() {
-    $httpBackend.flush();
+    it('should have one user', function() {
+      $httpBackend.flush();
 
-    expect(scope.users.length).toBe(1);
+      expect(scope.users.length).toBe(1);
+    });
   });
 });
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -5,10 +5,22 @@
 describe('service', function() {
   beforeEach(module('cuckooApp.services'));
 
-
   describe('version', function() {
     it('should return current version', inject(function(version) {
       expect(version).toEqual('0.1');
     }));
+  });
+});
+
+describe('FollowUtils', function() {
+  var followUtils;
+
+  beforeEach(module('cuckooApp.services'));
+  beforeEach(inject(function(FollowUtils) {
+    followUtils = FollowUtils;
+  }));
+
+  it('should get follow text', function() {
+    expect(followUtils.getFollowText({"followed": true})).toEqual('Followed');
   });
 });
